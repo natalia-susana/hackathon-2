5e89d4b82df595d4f65d58a0d28a017a1cbc29bc
OscarAndres008 <osmilo008@gmail.com>
2026-08-03 13:15:08 -0600

    metodo agregar usuarios

--- Agenda/src/servicie/Agenda.java
+++ Agenda/src/servicie/Agenda.java
@@ -1,4 +1,19 @@
 package servicie;
+import java.util.HashSet;
 
 public class Agenda {
+
+    private HashSet<Contacto> contactos;
+    private final int capacidadMaxima;
+
+    //asignar capacidad maxima en 10
+    public Agenda() {
+        contactos = new HashSet<>();
+        capacidadMaxima = 10;
+    }
+
+    // Método para agregar contactos
+    public void añadirContacto(Contacto contacto) {
+        contactos.add(contacto);
+    }
 }
--- Agenda/src/ui/Menu.java
+++ Agenda/src/ui/Menu.java
@@ -1,4 +1,7 @@
 package ui;
-
+import java.util.Scanner;
 public class Menu {
+    public static void main(String[] args) {
+
+    }
 }
