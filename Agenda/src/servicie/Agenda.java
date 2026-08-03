378708171fe4c27c566c4ecfa97d3bd8a8ab8461
KarenLunaS <karen.luna.salmeron3@gmail.com>
2026-08-03 15:46:05 -0600

    Agregando método de para modificar telefono

--- Agenda/src/servicie/Agenda.java
+++ Agenda/src/servicie/Agenda.java
@@ -1,5 +1,7 @@
 package servicie;
 import java.util.HashSet;
+import java.util.Scanner;
+import model.Contacto;
 
 public class Agenda{
 
@@ -38,5 +40,35 @@ public class Agenda{
     // Método para agregar contactos
     public void añadirContacto(Contacto contacto) {
         contactos.add(contacto);
+    }
+
+
+    // Metodo para modificar teléfono
+    public void modificarTelefono(String nombre, String apellido, String nuevoTelefono) {
+        Scanner sc = new Scanner(System.in);
+
+        // Recorremos la agenda para verificar si el contacto existe
+        for (Contacto persona : contactos) {
+            if (persona.getNombre().equalsIgnoreCase(nombre) && persona.getApellido().equalsIgnoreCase(apellido)) {
+
+                // verificamos que el parametro de numero contenga un valor, si es así, le quitamos signos extra y solo quedan digitos
+                String telefonoValido = (nuevoTelefono != null) ? nuevoTelefono.replaceAll("\\D", "") : "";
+
+                // Si no tiene 10 dígitos, solicitamos la corrección en bucle
+                while (telefonoValido.length() != 10) {
+                    System.out.println("Número inválido. El número ingresado debe tener 10 dígitos.");
+                    System.out.print("Ingresa el nuevo número: ");
+                    telefonoValido = sc.nextLine().replaceAll("\\D", "");
+                }
+
+                // Guardamos el número hasta que cumpla la condición de 10 digitos
+                persona.setTelefono(telefonoValido);
+                System.out.println("Teléfono actualizado con éxito.");
+                return;
+            }
+        }
+
+        //si nombre y apellido no hacen match con los contactos guardados, enviamos mensaje
+        System.out.println("Ese contacto no existe en tu agenda.");
     }
 }
