38a2ab57ea7997d51fd116bbefb18ea91fc437b3
Missael <manjarrezmissael@gmail.com>
2026-08-03 16:08:08 -0600

    Cambios en agenda

--- Agenda/src/servicie/Agenda.java
+++ Agenda/src/servicie/Agenda.java
@@ -1,33 +1,11 @@
 package servicie;
 import java.util.HashSet;
-
+import model.Contacto;
 public class Agenda{
 
-    //Buscar por nombre y apellido, si existe, mostrar telefono
-    //Arreglo
-    private Contacto[] contactos;
-    // Constructor
-    public Agenda(int capacidad) {
-        this.contactos = new Contacto[capacidad];
-    }
-    public String buscarContacto(String nombre, String apellido) {
-        // Validación
-        if (nombre == null || apellido == null || nombre.trim().isEmpty() || apellido.trim().isEmpty()) {
-            return "El nombre y apellido no pueden estar vacíos.";
-        }
-        for (Contacto c : contactos) {
-            if (c != null) {
-                if (c.getNombre().equalsIgnoreCase(nombre) && c.getApellido().equalsIgnoreCase(apellido)) {
-                    return "Teléfono: " + c.getTelefono();
-                }
-            }
-        }
-        return "Contacto no encontrado: " + nombre + " " + apellido;
-    }
-}
 
     private HashSet<Contacto> contactos;
-    private final int capacidadMaxima;
+    private int capacidadMaxima;
 
     //asignar capacidad maxima en 10
     public Agenda() {
@@ -38,5 +16,27 @@ public class Agenda{
     // Método para agregar contactos
     public void añadirContacto(Contacto contacto) {
         contactos.add(contacto);
+    }
+
+    //Buscar por nombre y apellido, si existe, mostrar telefono
+    //Arreglo
+    private Contacto[] contactoss;
+    // Constructor
+    public Agenda(int capacidad) {
+        this.contactoss = new Contacto[capacidad];
+    }
+    public String buscarContacto(String nombre, String apellido) {
+        // Validación
+        if (nombre == null || apellido == null || nombre.trim().isEmpty() || apellido.trim().isEmpty()) {
+            return "El nombre y apellido no pueden estar vacíos.";
+        }
+        for (Contacto c : contactoss) {
+            if (c != null) {
+                if (c.getNombre().equalsIgnoreCase(nombre) && c.getApellido().equalsIgnoreCase(apellido)) {
+                    return "Teléfono: " + c.getTelefono();
+                }
+            }
+        }
+        return "Contacto no encontrado: " + nombre + " " + apellido;
     }
 }
